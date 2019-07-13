942c86dde8703642f53ee617ce7e6b22455f73fe
杰少帅 <jie305998@163.com>
2019-07-13 16:52:00 +0800

    tabs tab

--- src/components/CategorySelect.js
+++ src/components/CategorySelect.js
@@ -1,52 +1,36 @@
 import React from 'react'
+import Ionicon from 'react-ionicons'
+import PropTypes from 'prop-types'
+import { Colors } from '../utility'
 
 class CategorySelect extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            selectedCategoryId: props.selectedCategoryId && props.selectedCategoryId.id
-
-        }
-    }
-
     selectCategory = (event, category) => {
-
-        this.setState({
-            selectedCategoryId:category.id
-
-        })
         this.props.onSelectCategory(category)
         event.preventDefault()
     }
-
     render() {
-        const {categories} = this.props
-        const {selectedCategory} = this.state
+        const { categories, selectedCategory } = this.props
+        const selectedCategoryId = selectedCategory && selectedCategory.id
         return (
-            <div className="category-select-Component">
+            <div className="category-select-component">
                 <div className="row">
                     {
                         categories.map((category, index) => {
-                            const activeClassName = (selectedCategory === category.id)
+                            const iconColor = (category.id === selectedCategoryId) ? Colors.white : Colors.gray
+                            const backColor = (category.id === selectedCategoryId) ? Colors.blue : Colors.lightGray
+                            const activeClassName = (selectedCategoryId === category.id)
                                 ? 'category-item col-3 active' : 'category-item col-3'
-
                             return (
-
-                                <div className={activeClassName}
-                                     onClick={(event) => {
-                                         this.selectCategory(event, category)
-                                     }}
-
-                                     key={index}>
+                                <div className={activeClassName} key={index} role="button" style={{ textAlign: 'center'}}
+                                     onClick={(event) => {this.selectCategory(event, category)}}>
                                     <Ionicon
                                         className="rounded-circle"
+                                        style={{ backgroundColor: backColor, padding: '5px' }}
                                         fontSize="50px"
-                                        color="#555"
+                                        color={iconColor}
                                         icon={category.iconName}
-
-                                    >
-
-                                    </Ionicon>
+                                    />
+                                    <p>{category.name}</p>
                                 </div>
                             )
                         })
@@ -55,5 +39,11 @@ class CategorySelect extends React.Component {
             </div>
         )
     }
-
 }
+
+CategorySelect.propTypes = {
+    categories: PropTypes.array.isRequired,
+    selectedCategory: PropTypes.object,
+    onSelectCategory: PropTypes.func.isRequired,
+}
+export default CategorySelect
--- src/containers/create.js
+++ src/containers/create.js
@@ -1,8 +1,52 @@
 import React from 'react'
 import Ionicon from 'react-ionicons'
+import {testCategories} from "../testData";
+import {TYPE_OUTCOME} from "../utility";
+import {Tabs,Tab} from '../components/Tabs'
+import CategorySelect from '../components/CategorySelect'
+import PriceForm from '../components/PriceForm'
 
-const Create=({match})=>{
-    return <h1>this this my create page{match.params.id}</h1>
+class Create extends React.Component {
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            selectedTab: TYPE_OUTCOME,
+            selectedCategory: null,
+            validationPassed: true,
+        }
+
+    }
+    render(){
+        const { selectedTab, selectedCategory, validationPassed } = this.state
+
+        const filterCategories=testCategories.filter(category =>category.type ===TYPE_OUTCOME)
+        return (
+            <div className="create-page py-3 px-3 rounded mt-3" style={{backgroundColor:'#fff'}}>
+                <Tabs activeIndex={0} onTabChange={()=>{}}>
+                    <Tab>支出</Tab>
+                    <Tab>收入</Tab>
+
+
+                </Tabs>
+                <CategorySelect
+                    categories={filterCategories}
+                    onSelectCategory={()=>{}}
+                    selectedCategory={selectedCategory}
+                >
+
+                </CategorySelect>
+                <PriceForm
+                    onFormSubmit={()=>{}}
+
+                    onCancelSubmit={()=>{}}>
+
+                </PriceForm>
+
+
+            </div>
+        )
+    }
+
 }
-
 export default Create
